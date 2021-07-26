2de30f87c0b5d958b00705a9b0ceaaeb1cb334a9
Gilles Dongmo <perrydongmo@gmail.com>
2021-07-25 23:56:43 -0400

    added a html file to run the script as well as added an object named perry

--- datatypes_strings.js
+++ datatypes_strings.js
@@ -1,7 +1,14 @@
 //data types
+let perry = {
+  name = 'Gilles Dongmo',
+  firstName = 'Gilles',
+  lastName = 'Dongmo',
+  age = 16, // number datatype
+  height = 5'11,
+  skinColor =; //undefines
+}; //this is an object datatype
 
-
-
+console.log(perry);
 
 
 
